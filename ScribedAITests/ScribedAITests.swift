b3f34ccd111d1185cd9c5819f003bd7da44c8162
uakbr <umair@tesla.com.ai>
2024-11-22 02:27:24 -0600

    Initial Commit

---
+++ ScribedAITests/ScribedAITests.swift
@@ -0,0 +1,17 @@
+//
+//  ScribedAITests.swift
+//  ScribedAITests
+//
+//  Created by u on 11/22/24.
+//
+
+import Testing
+@testable import ScribedAI
+
+struct ScribedAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
